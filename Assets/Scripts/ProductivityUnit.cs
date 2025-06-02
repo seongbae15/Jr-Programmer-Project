7fe218b4d27111bc4720c815a5ea7686dfbc6eb9
seongbae <pseongbae@gmail.com>
2025-06-02 17:21:26 +0900

    Inheritance and polymorphism

---
+++ Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
+    protected override void BuildingInRange()
+    {
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+                Debug.Log(m_CurrentPile.ProductionSpeed);
+
+            }
+        }
+    }
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+
+    public override void GoTo(Building target)
+    {
+        ResetProductivity();
+        base.GoTo(target);
+    }
+
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+}
